41b917e905aab841d9fb74614544f8df41c23273
Alena Gritsiuk <alena22292@mail.ru>
2021-02-25 17:50:27 +0100

    Add a function to check if there are any anagrams of the given word

--- tasksJS/codewars_tasks.js
+++ tasksJS/codewars_tasks.js
@@ -5,3 +5,19 @@ function getCount(str) {
 }
 
 // *******************************
+
+// 2) Anagrams(word, array of words)
+
+function anagrams(word, words) {
+  let resultArray = [];
+  const sortedWord = word.split('').sort().join('');
+
+  for (let i = 0; i < words.length; i++){
+    if (words[i].split('').sort().join('') === sortedWord) {
+      resultArray.push(words[i]);
+    }
+  }
+  return resultArray;
+}
+// *******************************
+
