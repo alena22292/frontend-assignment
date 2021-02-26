03d6244592e1982a44c490d3b75ebb9e5246e4ab
Alena Gritsiuk <alena22292@mail.ru>
2021-02-26 16:40:24 +0100

    Add a function to check if the provided num can be in fib num *

--- tasksJS/fibonacci_num.js
+++ tasksJS/fibonacci_num.js
@@ -1,8 +1,17 @@
-// how to find a fibonacci num:
+function productFib(prod){
+  let a = 0;
+  let bNext = 1;
+  while(a*bNext < prod){
+    // the consistency is important: first change the highest num than the prev. num:
+    bNext = bNext + a;
+    a = bNext - a;
+  }
+  // Test: how to find a fibonacci num:
   // the first two nums of Fib range:
   function Fib(n) {
      let s = 1;
      let m = 1;
+    // i = 3 because of the s and m assignment;
      for (let i = 3; i <= n; i++){
        let x = s + m;
        s = m;
@@ -10,6 +19,10 @@
      }
     return m;
   }
-  // console.log(Fib(a));
-  // console.log(Fib(bNext));
-  // console.log(Fib(a)*Fib(bNext));
+  console.log(Fib(a));
+  console.log(Fib(bNext));
+  console.log(Fib(a)*Fib(bNext));
+  return [a, bNext, a*bNext === prod];
+}
+
+// => productFib(4895), [55, 89, true]
