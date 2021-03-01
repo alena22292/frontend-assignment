013c515fa1cacc9c8fbb9dfe776515fbd9c8632d
Alena Gritsiuk <alena22292@mail.ru>
2021-03-01 14:51:59 +0100

    Comments for the task

--- tasksJS/react_form.js
+++ tasksJS/react_form.js
@@ -10,7 +10,14 @@ class WishlistForm extends React.Component {
       <form >
          <input id="name" type="text" placeholder="Your name" />
          <textarea id="wish" placeholder="Describe your wish" />
+         <select>
+           <option />
+         </select>
       </form>
     );
   }
 };
+
+
+// an input field for the child's name (with id 'name')
+// a text area to describe the wish (id: 'wish')
