eae06dfd185b74bbbe0192e7bdcd072e4dd29376
Alena Gritsiuk <alena22292@mail.ru>
2021-03-07 12:34:38 +0100

    Add an alternative function without reduceer

--- tasksJS/codewars_tasks.js
+++ tasksJS/codewars_tasks.js
@@ -292,6 +292,15 @@ function solution(input, markers) {
   return input.split('\n').map(line => markers.reduce((line, marker) => line.split(marker)[0].trim(), line)).join('\n');
 };
 
+function solution(input, markers)
+{
+  var lines = input.split("\n");
+  for (var i = 0; i < lines.length; ++i)
+    for (var j = 0; j < markers.length; ++j)
+      lines[i] = lines[i].split(markers[j])[0].trim();
+  return lines.join("\n");
+}
+
 // => solution("apples, pears # and bananas\ngrapes\nbananas !apples!, adrians ^ ", ["#", "!", "^"])
 
 // output => "apples, pears
