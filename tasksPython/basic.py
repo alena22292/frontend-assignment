907e8e5387c76c220288796ae786f3686528be4f
Alena Gritsiuk <alena22292@mail.ru>
2021-03-24 18:21:37 +0100

    Method to converting the weight

--- tasksPython/basic.py
+++ tasksPython/basic.py
@@ -23,4 +23,20 @@ if good_credit:
 else:
     total = price * one_percent
     print("The down payment", total)
-print(f"Actually, you may use local variable outside the block code, here it is the value of total: {total}")
+print(f"Actually, you may use local variable outside the block code, here it is the value of total: ${total}")
+
+# _________________________
+# Convering the weight (kg and lb)
+weight = input("Please inter your Weight ")
+x = input("(K)-kg or (L)-lb ")
+
+if x.lower() == "l":
+    total = int(weight) * 0.45
+    print(f"You weight in kg: {round(total)}")
+elif x.lower() == "k":
+    total = int(weight) * 2.2
+    print(f"You weight in lb: {round(total)}")
+else:
+    print(f"Invalid input")
+
+    
