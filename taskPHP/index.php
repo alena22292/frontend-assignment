f2951b5da16544b0dc11c0f3b3b3817d92578c65
Alena Gritsiuk <alena22292@mail.ru>
2021-04-10 14:53:56 +0100

    Complex Type Casting and Overwriting example

--- taskPHP/index.php
+++ taskPHP/index.php
@@ -115,6 +115,26 @@
 
         // Associative array (pair of key and values):
         $grades = array("Alena" => "A+", "Next" => "B");
+
+        // take a note about key assignment:
+
+        $array = array(
+            1    => 'a',
+            '1'  => 'b', // the value "a" will be overwritten by "b"
+            1.5  => 'c', // the value "b" will be overwritten by "c"
+            -1 => 'd',
+            '01'  => 'e', // as this is not an integer string it will NOT override the key for 1
+            '1.5' => 'f', // as this is not an integer string it will NOT override the key for 1
+            true => 'g', // the value "c" will be overwritten by "g"
+            false => 'h',
+            '' => 'i',
+            null => 'j', // the value "i" will be overwritten by "j"
+            'k', // value "k" is assigned the key 2. This is because the largest integer key before that was 1
+            2 => 'l', // the value "k" will be overwritten by "l"
+        );
+
+        var_dump($array);
+           
         
         // methods: 
         count($grades); 
