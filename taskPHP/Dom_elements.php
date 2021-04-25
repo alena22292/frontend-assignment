0b0cfef0323403228b0e4ef382cd5b4c894f0d3a
Alena Gritsiuk <alena22292@mail.ru>
2021-04-25 15:52:13 +0200

    DOM if-else statement

--- taskPHP/Dom_elements.php
+++ taskPHP/Dom_elements.php
@@ -18,11 +18,11 @@
   <script>
     let myFunction = function(){
       // Add event listeners to element on the page;
-      document.querySelector('.list-view').addEventListener('click', onClick);
+      document.querySelector('.list-view').addEventListener('click', onClick2);
       // Target will be ul;
       let list = document.querySelectorAll('list-item');
       list.forEach(li => {
-        li.addEventListener('click', onClick);
+        // li.addEventListener('click', onClick2);
         // Target will be li;
       })
       let spans = document.querySelectorAll('.list-item span');
@@ -36,6 +36,25 @@
       h2.textContent = id + ': ' + name;
       // ISSUE is you can't predict what will be used(clicked). Make sure a user will click on the li element neither 'span' or 'ul' as current target;
       // function onClick2 will solve the problem;
+    }
+
+    let onClick2 = function(e) {
+      let li = e.target;
+      if(e.target.tagName == 'SPAN'){
+        li = e.target.parentElement;
+      }
+      if(e.target.tagName == 'UL') {
+        // Not right way to solve it becouse every time will show the first element of the array:
+        // li = e.target.children[0];
+        // console.log(li.getAttribute('data-name'));
+        alert('Please chose one of the following items')
+      }
+      // alert(li.tagName);
+      // Should to be LI whether e.target == 'SPAN' or Not;
+      let id = li.getAttribute('data-id');
+      let name = li.getAttribute('data-name');
+      let h2 = document.querySelector('h2');
+      h2.textContent = `${id}: ${name}`;
     }
 
     document.addEventListener('DOMContentLoaded', myFunction);
