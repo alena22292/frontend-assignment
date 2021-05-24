3cfccd91c79777ef165c1689d21408cb86f26836
Alena Gritsiuk <alenagritsiuk@Alenas-MacBook-Air.local>
2021-05-24 12:55:49 +0200

    Create a constant variables for deefining DB setup connection

--- taskPHP/mySQL/db/connect.php
+++ taskPHP/mySQL/db/connect.php
@@ -1,5 +1,11 @@
 <?php 
-  //  mysql_connect('localhost name', 'username', 'password')
+  // set up the data base info in the constant variables
+  DEFINE("DB_USER", "studentweb");
+  DEFINE("DB_PASSWORD", "root");
+  DEFINE("DB_HOST", "localhost");
+  DEFINE("DB_NAME", "names");
+
+  //  mysql_connect('localhost name', 'username', 'password') must be separated from the other files
   mysql_connect('localhost', 'root', '');
   mysql_select_db('DB_name');
  ?>
